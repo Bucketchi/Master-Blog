e21c06a3885ccef319a92ffc5cecef36937e141d
Bucketchi <alexandretcoutinho@gmail.com>
2023-06-19 01:38:20 +0100

    Finalized all the basic functions and improved aesthetics

--- app.py
+++ app.py
@@ -2,24 +2,39 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 app = Flask(__name__)
 
+BLOG_FILE = "blog_posts.json"
+
+
+def get_blog_posts(filename):
+    """
+    Returns the list of blog posts from the storage file
+    by taking the filename as parameter.
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
+
 
 @app.route('/')
 def index():
-    with open("blog_posts.json", "r") as file:
-        blog_posts = json.load(file)
+    """
+    Shows the main page of the blog.
+    """
+    blog_posts = get_blog_posts(BLOG_FILE)
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-
+    """
+    Shows the form for adding a new post if GET or
+    posts the form if POST.
+    """
     if request.method == 'POST':
         author = request.form["author"]
         title = request.form["title"]
         content = request.form["content"]
 
-        with open("blog_posts.json", "r") as file:
-            blog_posts = json.load(file)
+        blog_posts = get_blog_posts(BLOG_FILE)
 
         new_id = 1
 
@@ -45,8 +60,10 @@ def add():
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
-    with open("blog_posts.json", "r") as file:
-        blog_posts = json.load(file)
+    """
+    Deletes the post with the ID passed to the function.
+    """
+    blog_posts = get_blog_posts(BLOG_FILE)
 
     for i, post in enumerate(blog_posts):
         if post["id"] == post_id:
@@ -56,6 +73,32 @@ def delete(post_id):
         file.write(json.dumps(blog_posts))
 
     return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    """
+    Shows the update form for the post with the ID passed
+    as an argument if GET or updates the post if POST.
+    """
+    blog_posts = get_blog_posts(BLOG_FILE)
+    for post in blog_posts:
+        if post["id"] == post_id:
+            selected_post = post
+
+    if request.method == 'POST':
+        selected_post["author"] = request.form["author"]
+        selected_post["title"] = request.form["title"]
+        selected_post["content"] = request.form["content"]
+
+        for i, post in enumerate(blog_posts):
+            if selected_post["id"] == post["id"]:
+                blog_posts[i] = selected_post
+        with open("blog_posts.json", "w") as file:
+            file.write(json.dumps(blog_posts))
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=selected_post)
 
 
 if __name__ == '__main__':
